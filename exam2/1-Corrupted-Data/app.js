3500885e1b2c2fbd4cd8426d00f2c232ef20278e
mimi0604 <mimi.bouyuklieva@gmail.com>
2014-07-25 18:32:37 +0300

    Exam 2

---
+++ exam2/1-Corrupted-Data/app.js
@@ -0,0 +1,14 @@
+ var data = require("./data");
+  var checkins = [ ];
+  data.forEach(function( details ) {
+
+       if (!checkins[details.fields.student + details.fields.date]){
+          checkins[details.fields.student + details.fields.date] = 1;
+       } else {
+          console.log(details);
+       }
+
+  });
+
+
+
